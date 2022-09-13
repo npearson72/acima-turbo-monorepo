edfd409a9d2f3af73299342e7e415a3b9ce967a3
Nathan Pearson <npearson72@gmail.com>
2022-09-12 19:50:47 -0700

    Fixes problems with Ionic source maps

--- apps/mobile/src/App.tsx
+++ apps/mobile/src/App.tsx
@@ -15,25 +15,13 @@ import {
 import { IonReactRouter } from '@ionic/react-router';
 import { fileTray, personCircle, barChart } from 'ionicons/icons';
 import { css } from '@emotion/react';
+
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
 
-/* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
-
-/* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
-
-/* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+// Imports default Ionic SCSS files
+import './App.scss';
 
 const style = css`
   text-align: center;
