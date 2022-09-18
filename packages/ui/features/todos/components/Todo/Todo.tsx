1a73a225488148106a4dca45660dc398e9bf9350
Nathan Pearson <npearson72@gmail.com>
2022-09-18 03:21:09 -0700

    Integrates update/patch call to server

--- packages/ui/features/todos/components/Todo/Todo.tsx
+++ packages/ui/features/todos/components/Todo/Todo.tsx
@@ -1,7 +1,9 @@
 import { useState } from 'react';
 import { isPlatform } from '@ionic/react';
+import { useQueryClient, useMutation } from '@tanstack/react-query';
 import TodoMobile from './Todo.mobile';
 import TodoWeb from './Todo.web';
+import { todosRepo } from '../../repos';
 
 interface Props {
   id: number;
@@ -11,13 +13,22 @@ interface Props {
 }
 
 const Todo: React.FC<Props> = props => {
-  const { complete, story } = props;
+  const { id, complete, story } = props;
   const [isChecked, setIsChecked] = useState(complete);
+
+  const queryClient = useQueryClient();
+  const mutation = useMutation(todosRepo.update, {
+    onSuccess: () => {
+      queryClient.invalidateQueries(['todos']);
+    }
+  });
 
   const handleCheck = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { checked } = event.currentTarget;
 
     setIsChecked(checked);
+
+    mutation.mutate({ id, complete: checked });
   };
 
   if (story?.mobile || isPlatform('mobile')) {
--- packages/ui/features/todos/repos/todosRepo.ts
+++ packages/ui/features/todos/repos/todosRepo.ts
@@ -5,6 +5,12 @@ const todosRepo = {
     const { data } = await httpClient.get('todos');
 
     return data;
+  },
+
+  async update(params: Record<string, any>) {
+    const { data } = await httpClient.patch(`todos/${params.id}`, params);
+
+    return data;
   }
 };
 
