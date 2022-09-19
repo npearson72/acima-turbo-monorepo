72cd6d181806112b41899efd5a2c4a328ab25d70
Nathan Pearson <npearson72@gmail.com>
2022-09-19 03:22:50 -0700

    Disables refetchOnWindowFocus on React Query

--- packages/ui/features/todos/index.tsx
+++ packages/ui/features/todos/index.tsx
@@ -1,7 +1,13 @@
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { TodosView } from './views';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false
+    }
+  }
+});
 
 const Todos: React.FC = () => {
   return (
