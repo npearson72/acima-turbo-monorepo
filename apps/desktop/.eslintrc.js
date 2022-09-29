7ba4de620f581ed39a80491fee5da6cb6ab64d00
Nathan Pearson <npearson72@gmail.com>
2022-09-29 05:09:07 -0700

    Fixes ESLint in Desktop

--- apps/desktop/.eslintrc.js
+++ apps/desktop/.eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  extends: ['@acima/custom'],
+  extends: ['@acima/custom/react'],
   parserOptions: {
     project: ['./tsconfig.json', '../../tsconfig.eslint.json'],
     tsconfigRootDir: __dirname
