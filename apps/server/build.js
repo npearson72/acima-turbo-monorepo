13aafe0f337ac240d5b2ab92691d2ddf4d38c625
Nathan Pearson <npearson72@gmail.com>
2022-10-05 01:52:46 -0700

    Replaces tsx with nodemon and concurrently

--- apps/server/build.js
+++ apps/server/build.js
@@ -1,14 +1,35 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-require('esbuild')
+const fs = require('fs');
+const esbuild = require('esbuild');
+const yargs = require('yargs-parser');
+
+const { _, ...argv } = yargs(process.argv.slice(2)) || {};
+
+esbuild
   .build({
     bundle: true,
     entryPoints: ['src/index.ts'],
     keepNames: true,
     logLevel: 'error',
-    minify: true,
     outfile: 'dist/index.js',
     platform: 'node',
     sourcemap: 'linked',
-    sourcesContent: false
+    sourcesContent: true,
+    ...argv
+  })
+  .then(() => {
+    if (!fs.existsSync('dist')) {
+      fs.mkdirSync('dist');
+    }
+
+    if (!fs.existsSync('dist/schema.prisma')) {
+      fs.copyFileSync('prisma/schema.prisma', 'dist/schema.prisma');
+    }
+
+    if (!fs.existsSync('dist/libquery_engine-darwin.dylib.node')) {
+      fs.copyFileSync(
+        '../../node_modules/.prisma/client/libquery_engine-darwin.dylib.node',
+        'dist/libquery_engine-darwin.dylib.node'
+      );
+    }
   })
   .catch(() => process.exit(1));
